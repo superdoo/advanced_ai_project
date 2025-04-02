d77d81e2fb6c6c2e3dd83963574cf60d5f892b0d
mbarreras <wbbmb2016@gmail.com>
2025-04-02 07:57:25 -0500

    ok

--- train_model.py
+++ train_model.py
@@ -33,7 +33,7 @@ import psycopg2
 def get_db_connection():
     return psycopg2.connect(
         dbname="mbarreras_db",
-        user="your_user",
+        user="mbarreras",
         password="B133eras",
         host="localhost",
         port="5432"
