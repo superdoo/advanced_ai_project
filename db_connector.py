8ba40e9f4cc80f0957620279e35abd1bd304c8f6
mbarreras <wbbmb2016@gmail.com>
2025-04-07 15:26:06 -0500

    Updating to clean up commits and make this available for others

--- db_connector.py
+++ db_connector.py
@@ -1,8 +1,8 @@
 def get_db_connection():
     return psycopg2.connect(
         dbname="mbarreras_db",
-        user="mbarreras",
-        password="B133eras",
+        user="**********",
+        password="*********",
         host="localhost",
         port="5432"
     )
